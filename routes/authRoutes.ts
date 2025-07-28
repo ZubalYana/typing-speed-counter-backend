254841ab0c60b5b98943295c45f1a56edc87c5a8
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-07-28 05:39:01 +0300

    create signing up base with all the authorization standarts

---
+++ routes/authRoutes.ts
@@ -0,0 +1,45 @@
+import { Router, Request, Response } from 'express'
+import UserModel from '../models/User'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const router = Router();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret'
+
+router.post('/signUp', async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await UserModel.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ message: 'Email is already in use' })
+        };
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = new UserModel({ name, email, password: hashedPassword });
+        await newUser.save()
+
+        const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, {
+            expiresIn: '7d',
+        });
+
+        res.status(201).json({
+            message: 'User created successfully',
+            token,
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            },
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500).json({ message: 'Internal server error' })
+    }
+})
